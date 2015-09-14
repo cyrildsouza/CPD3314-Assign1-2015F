e90edbc5c6b843d177eac7cf74d9d4096fe741f0
Cyril Dsouza <c06638432@mylambton.ca>
2015-09-14 14:16:50 -0400

    Added to CPD3314-Assign1-2015F

--- src/cpd3314/assign1/CPD3314Assign1.java
+++ src/cpd3314/assign1/CPD3314Assign1.java
@@ -17,7 +17,7 @@ public class CPD3314Assign1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
-    /* Assignment 1
+    /* Assignment 1*/
     }
     
 }
