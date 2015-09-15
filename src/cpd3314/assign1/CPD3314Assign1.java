f10d4a961d96f63754c6734a725c494b21000b09
Cyril Dsouza <c06638432@mylambton.ca>
2015-09-15 17:07:26 -0400

    Adding the assignment

--- src/cpd3314/assign1/CPD3314Assign1.java
+++ src/cpd3314/assign1/CPD3314Assign1.java
@@ -17,7 +17,35 @@ public class CPD3314Assign1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
-    /* Assignment 1*/
+    /* Assignment 1
+        1. Job Posting with the word "JAVA" in it.
+        Ans.http://www.indeed.ca/viewjob?jk=91e017b6db622606&q=Java&l=Canada&tk=19v64815ja4i8cr9&from=web
+            Java Developer
+            BLiNQ - Kanata, ON
+            Oracle certified Java Developer. 7+ years proven experience of Java, JEE development, preferably     in projects involving multi-tier enterprise systems...
+
+        2. Find a URL for and a short definition for every technology that they require. 
+        Ans. JEE - Java Platform, Enterprise Edition - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+             GWT - Google Web Toolkit - https://en.wikipedia.org/wiki/Google_Web_Toolkit
+             JavaScript - https://en.wikipedia.org/wiki/JavaScript
+             CSS - Cascading Style Sheets - https://en.wikipedia.org/wiki/Cascading_Style_Sheets
+             JSON - JavaScript Object Notation - https://en.wikipedia.org/wiki/JSON
+             SOAP - Simple Object Access Protocol - https://en.wikipedia.org/wiki/SOAP 
+             JAX-RS - Java API for RESTful Web Services - https://en.wikipedia.org/wiki/Java_API_for_RESTful_Web_Services
+             JSP - JavaServer Pages - https://en.wikipedia.org/wiki/JavaServer_Pages
+             JDBC - Java Database Connectivity - https://en.wikipedia.org/wiki/Java_Database_Connectivity
+             HSQLDB - Hyper SQL Database - https://en.wikipedia.org/wiki/HSQLDB
+             MySQL - My Sequel - https://en.wikipedia.org/wiki/MySQL
+             Hibernate - Hibernate Object relational mapping - https://en.wikipedia.org/wiki/Hibernate_(Java)
+             JQuery - jQuery is a cross-platform JavaScript library - https://en.wikipedia.org/wiki/JQuery
+             Bootstrap - Bootstrap is a free and open-source collection of tools for creating websites and web applications - https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)
+
+        3. Find a Potential Salary for that Job in Toronto, Vancouver and Waterloo.
+        Ans. TORONTO - C$60,189
+             Vancouver - C$82,707
+             Waterloo - C$71,080
+    */
+      
     }
     
 }
